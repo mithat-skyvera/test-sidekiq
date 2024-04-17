e7187ca1eec2b637c188fd302c4a415a17cfc75e
mithat-skyvera <mithat.turan@skyvera.com>
2024-04-17 15:02:42 +0300

    infinite sidekiq usage for testing memory

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    HardJob.perform_in(5.minutes.from_now)
-    HardJob.perform_async
+    # HardJob.perform_in(5.minutes.from_now)
+    # HardJob.perform_async
+
+    loop do
+      HardJob.perform_async
+      sleep 2
+    end
   end
 end
