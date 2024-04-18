d8e92920610b54f818dc1e08b4581cbf6f20c694
mithat-skyvera <mithat.turan@skyvera.com>
2024-04-18 12:02:44 +0300

    small array operations

--- app/sidekiq/hard_job.rb
+++ app/sidekiq/hard_job.rb
@@ -2,7 +2,7 @@ class HardJob
   include Sidekiq::Job
 
   def perform
-    # big_array = Array.new(1_000_000) {Object.new} 
+    small_array = Array.new(1000) {Object.new} 
     puts "Sidekiq HardJob is running"
   end
 end
