1af0eaf19688b709d79707e17ef4819fd048ba0c
mithat-skyvera <mithat.turan@skyvera.com>
2024-04-18 10:12:12 +0300

    remove array process in sidekiq

--- app/sidekiq/hard_job.rb
+++ app/sidekiq/hard_job.rb
@@ -2,7 +2,7 @@ class HardJob
   include Sidekiq::Job
 
   def perform
-    big_array = Array.new(1_000_000) {Object.new} 
+    # big_array = Array.new(1_000_000) {Object.new} 
     puts "Sidekiq HardJob is running"
   end
 end
