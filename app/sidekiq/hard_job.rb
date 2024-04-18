00b17e5504d3035659b33f70da357e763f8af16a
mithat-skyvera <mithat.turan@skyvera.com>
2024-04-18 09:32:09 +0300

    big array in sidekiq job

--- app/sidekiq/hard_job.rb
+++ app/sidekiq/hard_job.rb
@@ -2,8 +2,7 @@ class HardJob
   include Sidekiq::Job
 
   def perform
+    big_array = Array.new(1_000_000) {Object.new} 
     puts "Sidekiq HardJob is running"
-    array = Array.new(1_000_000) {rand}
-    puts "Sidekiq HardJob is done"
   end
 end
